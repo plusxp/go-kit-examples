037abf2b647e8551b774b0a373fc5f741237decc
Lei <randomleili@gmail.com>
2020-10-09 00:33:02 +0800

    add Prepare method

--- gokit_foundation/gateway/gateway.go
+++ gokit_foundation/gateway/gateway.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	gogojsonpb "github.com/gogo/protobuf/jsonpb"
 	gogoproto "github.com/gogo/protobuf/proto"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/mux"
 	"gokit_foundation"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -17,6 +19,26 @@ import (
 	"syscall"
 	"time"
 )
+
+const gatewayErrPrefix = "gateway:"
+
+//// 网关域内的err类型
+//type ErrCodeT int
+//
+//type ErrT struct {
+//	Code ErrCodeT
+//	Err  error
+//}
+//
+///*
+//网关域内的err_code常量，应该与于proto/common/resultcode.proto内的定义一致
+//但不需要全部包括，根据需要定义
+//*/
+//const (
+//	ECode_NoErr         ErrCodeT = iota
+//	ECode_JsonUnmarshal ErrCodeT = 7
+//	ECode_Auth          ErrCodeT = 8
+//)
 
 type Gateway struct {
 	r *mux.Router
@@ -104,11 +126,13 @@ func (g *Gateway) setupMW() {
 var defProtojsonpbM = &jsonpb.Marshaler{
 	EnumsAsInts:  true, // proto enum类型仍然转为整数，而不是代表其含义的str
 	EmitDefaults: true, // 仍然展示零值字段
+	OrigName:     true,
 }
 
 var defGogojsonpbM = &gogojsonpb.Marshaler{
 	EnumsAsInts:  true,
 	EmitDefaults: true,
+	OrigName:     true,
 }
 
 // JSON 直接封装+响应json数据.
@@ -120,9 +144,10 @@ func (g *Gateway) JSON(w http.ResponseWriter, rsp interface{}) error {
 	defer func() {
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			g.Log("Gateway.JSON: got err", err)
+			g.Log("Gateway.JSON: err", err)
 			return
 		}
+		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write(b)
 	}()
 
@@ -148,4 +173,50 @@ func (g *Gateway) JSON(w http.ResponseWriter, rsp interface{}) error {
 // 用于传递logger
 func (g *Gateway) RawLogger() *gokit_foundation.Logger {
 	return g.Logger
+}
+
+// Prepare执行身份验证，反序列化等操作
+// 当需要返回非200http状态码时，rsp.body为空即可
+func (g *Gateway) Prepare(w http.ResponseWriter, httpReq *http.Request, rpcReq interface{}) (ok bool) {
+	var err error
+	defer func() {
+		if err == nil {
+			ok = true
+			return
+		}
+		g.Log("Gateway.Prepare: err", err, "method", httpReq.Method, "path", httpReq.URL.Path)
+	}()
+
+	err = g.authenticate(httpReq)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if rpcReq != nil {
+		if err = g.unmarshalReq(httpReq, rpcReq); err != nil {
+			log.Print(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+	return
+}
+
+// 反序列化req
+func (g *Gateway) unmarshalReq(httpReq *http.Request, req interface{}) error {
+	return json.NewDecoder(httpReq.Body).Decode(req)
+}
+
+// 对req做身份验证
+func (g *Gateway) authenticate(httpReq *http.Request) error {
+	// JWT示例
+	return authViaJwt(httpReq)
+}
+
+// 网关域内的err应该有特定前缀
+func (g *Gateway) wrapErr(err error) error {
+	if err == nil {
+		return nil
+	}
+	return fmt.Errorf("%s %v", gatewayErrPrefix, err)
 }
