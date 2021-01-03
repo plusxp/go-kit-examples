9aa0cd1f5bf688df6b7072be7470b4de424149a5
Lei <randomleili@gmail.com>
2021-01-03 16:30:12 +0800

    refactor

--- demo_project/new_addsvc/cmd/addsvc/init.go
+++ demo_project/new_addsvc/cmd/addsvc/init.go
@@ -1,25 +1,107 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/go-kit/kit/log"
+	kitgrpc "github.com/go-kit/kit/transport/grpc"
 	"github.com/leigg-go/go-util/_redis"
+	stdopentracing "github.com/opentracing/opentracing-go"
+	"go-util/_util"
 	"gokit_foundation"
+	"google.golang.org/grpc"
+	"net"
+	"net/http"
 	"new_addsvc/config"
+	"new_addsvc/internal"
+	"new_addsvc/pb/gen-go/addsvcpb"
 	"new_addsvc/pkg/crontask"
-	"time"
+	"new_addsvc/pkg/endpoint"
+	"new_addsvc/pkg/service"
+	"new_addsvc/pkg/transport"
 )
 
-// 短时间的初始化任务，这种不能用g.Add
-func initFirstly(grpcHost string, grpcPort int) {
+func beforeStart() {
 	_redis.MustInitDef(config.GetRedisConf())
-	crontask.Init()
-	// 等一下，待所有内部服务准备就绪后再上线服务
-	// 1s表示所有内部服务需在1s内进入就绪状态，任何影响服务正常运行的错误都应该立即报错， 时间可根据完成具体初始化任务所需的时间来调整
-	time.AfterFunc(time.Second, func() {
-		gokit_foundation.MustRegisterSvc(config.SvcName, grpcHost, grpcPort, []string{"test"})
-	})
 }
-func onClose() {
+
+func afterStart(grpcHost string, grpcPort int) {
+	crontask.Init()
+	gokit_foundation.MustRegisterSvc(config.SvcName, grpcHost, grpcPort, []string{"test"}) // 后上线
+}
+
+func beforeStop() {
 	gokit_foundation.ConsulDeregister() // 先下线
 	crontask.Stop()
+}
+
+// 最后停止一些基础设施，一般是db相关、mq
+func afterStop() {
 	_redis.Close()
+}
+
+func startHttpSrv(mainCtx context.Context, httpSrvAddr string) <-chan struct{} {
+	closed := make(chan struct{})
+	httpSrv := &http.Server{}
+	stop := func() {
+		defer func() { closed <- struct{}{} }()
+		<-mainCtx.Done()
+		err := httpSrv.Shutdown(context.TODO())
+		//if err != nil {}
+		logger.Log(fmt.Sprintf("httpSrv(%s)", httpSrvAddr), "exited", "err", err)
+	}
+	go func() {
+		logger.Log("startHttpSrv", httpSrvAddr)
+
+		httpLis, err := net.Listen("tcp", httpSrvAddr)
+		_util.PanicIfErr(err, nil)
+
+		go stop()
+		// default use http.DefaultServeMux as handler
+		err = httpSrv.Serve(httpLis)
+		_util.PanicIfErr(err, []error{http.ErrServerClosed})
+	}()
+
+	return closed
+}
+
+func startgRPCSrv(mainCtx context.Context, grpcSrvAddr string) <-chan struct{} {
+	closed := make(chan struct{})
+	gRPCSrv := grpc.NewServer(grpc.UnaryInterceptor(kitgrpc.Interceptor))
+	stop := func() {
+		defer func() { closed <- struct{}{} }()
+		<-mainCtx.Done()
+		gRPCSrv.GracefulStop()
+		logger.Log(fmt.Sprintf("gRPCSrv(%s)", grpcSrvAddr), "exited")
+	}
+	go func() {
+		logger.Log("startgRPCSrv", grpcSrvAddr)
+		gRPCLis, err := net.Listen("tcp", grpcSrvAddr)
+		_util.PanicIfErr(err, nil)
+
+		addSrv := NewGokitSrv(logger)
+		addsvcpb.RegisterAddServer(gRPCSrv, addSrv)
+
+		gokit_foundation.RegistergRPCHealthSrv(gRPCSrv) // 这里注册healthSrv
+		go stop()
+		err = gRPCSrv.Serve(gRPCLis)
+		_util.PanicIfErr(err, nil)
+	}()
+
+	return closed
+}
+
+func NewGokitSrv(logger log.Logger) addsvcpb.AddServer {
+	metricsObj := internal.NewMetrics()
+	tracer := stdopentracing.GlobalTracer()
+
+	// 依次创建 svc，endpoint，transport三层的对象，每一层都会在上一层基础上封装
+	// 在svc和endpoint层以中间件的形式添加【指标上传、api日志】功能
+
+	// service需要的所有对象都通过New传入
+	svc := service.New(logger, metricsObj.Ints, metricsObj.Chars)
+	// 在endpoint层和transport层添加路径追踪功能
+	endpoints := endpoint.New(svc, logger, metricsObj.Duration, tracer)
+	addSrv := transport.NewgRPCServer(endpoints, tracer, logger)
+	return addSrv
 }
--- demo_project/new_addsvc/cmd/addsvc/main.go
+++ demo_project/new_addsvc/cmd/addsvc/main.go
@@ -5,37 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"github.com/go-kit/kit/log"
-	kitgrpc "github.com/go-kit/kit/transport/grpc"
-	stdopentracing "github.com/opentracing/opentracing-go"
-	"go-util/_go"
-	"go-util/_util"
 	"gokit_foundation"
-	"google.golang.org/grpc"
-	"net"
-	"net/http"
-	"new_addsvc/internal"
-	"new_addsvc/pb/gen-go/addsvcpb"
-	"new_addsvc/pkg/endpoint"
-	"new_addsvc/pkg/service"
-	"new_addsvc/pkg/transport"
 	"os"
-	"time"
+	"os/signal"
+	"syscall"
 )
-
-func NewAddSrv(logger log.Logger) addsvcpb.AddServer {
-	metricsObj := internal.NewMetrics()
-	tracer := stdopentracing.GlobalTracer()
-
-	// 依次创建 svc，endpoint，transport三层的对象，每一层都会在上一层基础上封装
-	// 在svc和endpoint层以中间件的形式添加【指标上传、api日志】功能
-
-	// service需要的所有对象都通过New传入
-	svc := service.New(logger, metricsObj.Ints, metricsObj.Chars)
-	// 在endpoint层和transport层添加路径追踪功能
-	endpoints := endpoint.New(svc, logger, metricsObj.Duration, tracer)
-	addSrv := transport.NewGRPCServer(endpoints, tracer, logger)
-	return addSrv
-}
 
 // for test
 func init() {
@@ -53,9 +27,7 @@ new_addsvc服务依赖了一些外部中间件如下：
 */
 
 var (
-	grpcSrv *grpc.Server
-	httpSrv *http.Server
-	logger  log.Logger
+	logger log.Logger
 )
 
 func main() {
@@ -70,80 +42,31 @@ func main() {
 	flag.Parse()
 	logger = gokit_foundation.NewKvLogger(nil)
 
-	grpcSrv = grpc.NewServer(grpc.UnaryInterceptor(kitgrpc.Interceptor))
-	httpSrv = &http.Server{}
+	beforeStart()
 
-	/*
-		这里使用 TaskGroup 完成程序的多任务同时启动，同时退出
-		在实际项目中可以参考其思路，自行实现
-	*/
+	// mainCtx传给每个后台运行的goroutine，mainCtx结束时，所有接收mainCtx的goroutine都应该退出
+	mainCtx, stopBackgroundTasks := context.WithCancel(context.TODO())
+	httpSrvClosed := startHttpSrv(mainCtx, httpSrvAddr)
+	gRPCSrvClosed := startgRPCSrv(mainCtx, grpcSrvAddr)
 
-	// 初始化一个TaskGroup对象
-	tg := _go.NewTaskGroup()
+	afterStart(srvHost, *grpcPort)
 
-	addTaskListenSignal(tg)
-	initFirstly(srvHost, *grpcPort)
+	logger.Log("gokit", "*******************Everything is ready*******************")
 
-	addTaskHttpSrv(tg, httpSrvAddr)
-	addTaskGRPCSrv(tg, grpcSrvAddr)
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
-	logger.Log("main", "started")
-	tg.Run()
-}
+	sig := <-quit
+	logger.Log("............recv-signal", sig)
 
-// 添加后台任务：监听退出信号（第一个添加）
-func addTaskListenSignal(tg *_go.TaskGroup) {
-	tk, sc := _util.ListenSignalTask(logger, onClose)
-	tg.Add(tk).Interrupt(func(err error) {
-		close(sc)
-	})
-}
-
-func addTaskHttpSrv(tg *_go.TaskGroup, httpSrvAddr string) {
-	// http服务监听8080, 目前只提供metric接口给prometheus调用
-	httpSrvTask := func(_ context.Context) error {
-		logger.Log("NewTaskGroup", "httpSrvTask", "httpSrvAddr", httpSrvAddr)
-
-		httpLis, err := net.Listen("tcp", httpSrvAddr)
-		_util.PanicIfErr(err, nil)
-
-		// default use http.DefaultServeMux as handler
-		err = httpSrv.Serve(httpLis)
-		return err
+	{
+		beforeStop()
+		stopBackgroundTasks() // 停止所有后台任务
+		afterStop()
 	}
-	tg.Add(httpSrvTask).Interrupt(func(err error) {
-		if err != nil {
-			logger.Log("httpSrvTask", "exited", "err", err)
-		} else {
-			closeCtx, _ := context.WithTimeout(context.Background(), time.Second*2)
-			err := httpSrv.Shutdown(closeCtx)
-			logger.Log("httpSrvTask", "exited", "clean", err)
-		}
-	})
-}
-
-func addTaskGRPCSrv(tg *_go.TaskGroup, grpcSrvAddr string) {
-	// 添加后台任务：启动rpc-srv
-	grpcSrvTask := func(_ context.Context) error {
-		logger.Log("NewTaskGroup", "grpcSrvTask", "grpcSrvAddr", grpcSrvAddr)
-
-		grpcLis, err := net.Listen("tcp", grpcSrvAddr)
-		_util.PanicIfErr(err, nil)
-
-		addSrv := NewAddSrv(logger)
-		addsvcpb.RegisterAddServer(grpcSrv, addSrv)
-		// 这里注册了AddSrv以及healthSrv
-		gokit_foundation.RegisterGRPCHealthSrv(grpcSrv)
-
-		err = grpcSrv.Serve(grpcLis)
-		return err
+	{ // 等待各个服务正常退出
+		<-httpSrvClosed
+		<-gRPCSrvClosed
 	}
-	tg.Add(grpcSrvTask).Interrupt(func(err error) {
-		if err != nil {
-			logger.Log("grpcSrvTask", "exited", "err", err)
-		} else {
-			grpcSrv.GracefulStop()
-			logger.Log("grpcSrvTask", "exited", "clean", nil)
-		}
-	})
+	logger.Log("gokit", "exited!")
 }
